1ae6f20b23767f421cd27ecfef97e505120ef75b
chhue <chhue96@gmail.com>
2023-05-23 17:51:20 +0900

    webview

--- app/src/main/java/com/ch96/tp1315/MainActivity.kt
+++ app/src/main/java/com/ch96/tp1315/MainActivity.kt
@@ -2,10 +2,51 @@ package com.ch96.tp1315
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import com.ch96.tp1315.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val homeUrl = "testhue96.dothome.co.kr/home.html/"
+    val shopUrl = "testhue96.dothome.co.kr/shop.html/"
+    val shareUrl = "testhue96.dothome.co.kr/share.html/"
+    val orderUrl = "testhue96.dothome.co.kr/order.html/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.webview.webViewClient = WebViewClient()
+        binding.webview.webChromeClient = WebChromeClient()
+        binding.webview.settings.javaScriptEnabled = true
+
+        binding.webview.loadUrl(homeUrl)
+
+
+        binding.bnv.setOnItemSelectedListener (NavigationBarView.OnItemSelectedListener {
+            changeBnv(it)
+            return@OnItemSelectedListener true
+        })
+
+    }
+
+    fun changeBnv(it: MenuItem){
+        when(it.itemId) {
+            R.id.home -> {
+                binding.webview.loadUrl(homeUrl ?: "")
+            }
+            R.id.shop -> {
+                binding.webview.loadUrl(shopUrl ?: "")
+            }
+            R.id.share -> {
+                binding.webview.loadUrl(shareUrl ?: "")
+            }
+            R.id.order -> {
+                binding.webview.loadUrl(orderUrl ?: "")
+            }
+        }
     }
 }
